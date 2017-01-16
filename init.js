36763b77992bdaa8784da5183290465e40a7ad4e
Dino <edin.cizmic@hotmail.com>
2017-01-16 18:38:07 +0100

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.slider').slider({full_width: true, height: 450});
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
