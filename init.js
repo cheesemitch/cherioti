cdec35a0f293441513fc86290c53c3b03c0d387b
Dino <edin.cizmic@hotmail.com>
2017-01-16 18:34:30 +0100

    Add files via upload

---
+++ init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({full_width: true, height: 450});
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
